bcae457596e2ab74233b972b1d3f759024734bc3
jokomakruf <jokomaruf88@gmail.com>
2019-08-28 21:36:41 +0700

    -

--- core/app/Controllers/TABLE/undangan.php
+++ core/app/Controllers/TABLE/undangan.php
@@ -3,5 +3,14 @@ if($action_request == "get"){
   $id_wedding = $_GET['id'];
   $sql = "SELECT * FROM undangan WHERE id_wedding = '$id_wedding'  ORDER BY nama";
   $data['data'] = $this->db->results($sql,'array');
-  $reply = json_encode($data);
+ 
+  if (!empty($data['data'])){
+
+    $reply = json_encode($data);
+    
+  }
+  else{
+    $data['data'] =null ;
+    $reply = json_encode($data);
+  }
 }
